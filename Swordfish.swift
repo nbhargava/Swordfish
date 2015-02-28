2d40db136bd6658f88b71f972a7cc6d09e24bdce
Nikhil Bhargava <nikhilb@stanford.edu>
2015-02-28 13:08:37 -0800

    - Switch to Swift 1.2 (beta) - Use class-level operation queue - Write to actual log file

--- Swordfish.swift
+++ Swordfish.swift
@@ -9,7 +9,32 @@ import Foundation
 import UIKit
 
 @objc class Swordfish {
+    static let logFilePath: String = {
+        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        let fileName = documentsDirectory.stringByAppendingPathComponent("swordfish.log")
+        return fileName
+    }()
+    
+    static var logFileHandle: NSFileHandle?
+    
+    static let operationQueue: NSOperationQueue = {
+        let queue = NSOperationQueue()
+        queue.name = "Swordfish"
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+    
     class func setupAnalytics() {
+        // Check if log file already exists. If so, attempt to upload it.
+        // If not, create the file
+        if NSFileManager.defaultManager().fileExistsAtPath(logFilePath) {
+            uploadLogFile()
+        } else {
+            NSFileManager.defaultManager().createFileAtPath(logFilePath, contents: nil, attributes: nil)
+            logFileHandle = NSFileHandle(forUpdatingAtPath: logFilePath)
+        }
+        
+        // swizzle methods
         method_exchangeImplementations(
             class_getInstanceMethod(UIViewController.self, "viewDidAppear:"),
             class_getInstanceMethod(UIViewController.self, "Swordfish_viewDidAppear:"))
@@ -19,6 +44,20 @@ import UIKit
     }
     
     class func log(eventMap: [String: AnyObject], withCategory category: String) {
-        println("\(category):\t\(eventMap)")
+        operationQueue.addOperationWithBlock { () -> Void in
+            let jsonDict = NSJSONSerialization.dataWithJSONObject([category: eventMap], options:NSJSONWritingOptions(0), error: nil)
+            self.logFileHandle?.writeData(jsonDict!)
+            self.logFileHandle?.writeData(("\n" as NSString).dataUsingEncoding(NSUTF8StringEncoding)!)
+        }
+    }
+    
+    private class func uploadLogFile() {
+        // TODO: Upload instead of delete
+        NSFileManager.defaultManager().removeItemAtPath(logFilePath, error: nil)
+        println("Swordfish - DELETING LOG FILE")
+        
+        // Create a new file afterwards
+        NSFileManager.defaultManager().createFileAtPath(logFilePath, contents: nil, attributes: nil)
+        logFileHandle = NSFileHandle(forUpdatingAtPath: logFilePath)
     }
 }
